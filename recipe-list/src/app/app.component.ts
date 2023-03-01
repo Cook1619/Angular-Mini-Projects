6ff223d04fdcf055a501362ae015dcbeaa337de5
Cook1619 <cook6348@gmail.com>
2023-03-01 16:12:17 -0600

    Setting up some ghetto navigation for the time being

--- recipe-list/src/app/app.component.ts
+++ recipe-list/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'recipe-list';
+  selection = 'recipe'
+
+  onHeaderChange(headerData){
+    this.selection = headerData
+  }
+
 }
