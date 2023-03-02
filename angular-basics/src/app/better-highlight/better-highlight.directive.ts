559e7ddec75c9e6693dbae9f18ecc4773bce8860
Cook1619 <cook6348@gmail.com>
2023-03-02 11:13:00 -0600

    A way to be able to pass custom values to custom directive instead of default ones defined in directive. Make note of the binding syntax for the input can be done either way when passing a string

--- angular-basics/src/app/better-highlight/better-highlight.directive.ts
+++ angular-basics/src/app/better-highlight/better-highlight.directive.ts
@@ -1,22 +1,25 @@
-import {Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostBinding, HostListener, Input, OnInit, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]',
 })
 export class BetterHighlightDirective implements OnInit{
-  @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent';
+  @Input() defaultColor: string = 'transparent'
+  @Input() highlightColor: string = 'blue'
+  @HostBinding('style.backgroundColor') backgroundColor: string;
   constructor(private renderer: Renderer2, private elementRef: ElementRef) {}
 
   ngOnInit() {
     // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue')
+    this.backgroundColor = this.defaultColor
   }
 
   @HostListener('mouseenter') mouseEnter(){
     // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'green')
-    this.backgroundColor = 'green'
+    this.backgroundColor = this.highlightColor
   }
   @HostListener('mouseleave') mouseLeave(){
     // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent')
-    this.backgroundColor = 'transparent'
+    this.backgroundColor = this.defaultColor
   }
 }
