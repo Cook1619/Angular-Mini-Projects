c98a50b37ecb2fc4b278f07016cc6511d64bc0f4
Cook1619 <cook6348@gmail.com>
2023-03-02 11:02:29 -0600

    A different way to change styles using host binding instead of using the renderer2

--- angular-basics/src/app/better-highlight/better-highlight.directive.ts
+++ angular-basics/src/app/better-highlight/better-highlight.directive.ts
@@ -1,19 +1,22 @@
-import {Directive, ElementRef, HostListener, OnInit, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]',
 })
 export class BetterHighlightDirective implements OnInit{
+  @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent';
   constructor(private renderer: Renderer2, private elementRef: ElementRef) {}
 
   ngOnInit() {
     // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue')
   }
 
-  @HostListener('mouseenter') mouseEnter(event: Event){
-    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'green')
+  @HostListener('mouseenter') mouseEnter(){
+    // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'green')
+    this.backgroundColor = 'green'
   }
-  @HostListener('mouseleave') mouseLeave(event: Event){
-    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent')
+  @HostListener('mouseleave') mouseLeave(){
+    // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent')
+    this.backgroundColor = 'transparent'
   }
 }
