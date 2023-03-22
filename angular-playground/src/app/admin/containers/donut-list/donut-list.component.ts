8c03ede3fb22acfd041870a8c9152fc68fa1d0c8
Cook1619 <cook6348@gmail.com>
2023-03-22 08:47:32 -0500

    creating a static donut card for now before implementing its own component which we pass data to

--- angular-playground/src/app/admin/containers/donut-list/donut-list.component.ts
+++ angular-playground/src/app/admin/containers/donut-list/donut-list.component.ts
@@ -3,14 +3,43 @@ import {Component, OnInit} from '@angular/core';
 @Component({
   selector: 'app-donut-list',
   template: `
-    <p>
-      donut-list works!
-    </p>
+    <div>
+      <div>
+        {{donut.name}}
+        {{donut.price}}
+      </div>
+    </div>
   `,
   styles: [
   ]
 })
 export class DonutListComponent implements OnInit {
-  ngOnInit() {
+  donut!: any;
+  donuts!: any[];
+  ngOnInit(): void {
+    this.donuts = [
+      {
+        id: 'as1234',
+        name: 'Chocolate',
+        icon: 'just-chocolate',
+        price: 119,
+        description: 'For chocolate lovers'
+      },
+      {
+        id: 'adsd234',
+        name: 'Glazed Fudge',
+        icon: 'glazed-fudge',
+        price: 129,
+        description: 'Perfect for all'
+      },
+      {
+        id: 'as1456hgf4',
+        name: 'Caramel Swirl',
+        icon: 'caramel-swirl',
+        price: 129,
+        description: 'Chocolate with caramel swirl'
+      }
+    ]
+    this.donut = this.donuts[0];
   }
 }
