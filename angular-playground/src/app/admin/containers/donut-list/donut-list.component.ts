f6834558f2ffa629fffa8e72cde804c316a633c9
Cook1619 <cook6348@gmail.com>
2023-03-29 07:42:04 -0500

    another way to conditionally render without reference variables

--- angular-playground/src/app/admin/containers/donut-list/donut-list.component.ts
+++ angular-playground/src/app/admin/containers/donut-list/donut-list.component.ts
@@ -7,9 +7,9 @@ import {Donut} from "../../models/donut.model";
   selector: 'app-donut-list',
   template: `
     <div>
-      <ng-container *ngIf="donuts.length; then cards; else noDonuts">
-      </ng-container>
-      <ng-template #cards>
+<!--      <ng-container *ngIf="donuts.length; then cards; else noDonuts">-->
+<!--      </ng-container>-->
+      <ng-template [ngIf]="donuts.length">
         <donut-card [donut]="donuts[0]"></donut-card>
         <donut-card [donut]="donuts[1]"></donut-card>
         <donut-card [donut]="donuts[2]"></donut-card>
@@ -27,28 +27,28 @@ export class DonutListComponent implements OnInit {
 
   ngOnInit(): void {
     this.donuts = [
-      {
-        id: 'as1234',
-        name: 'Chocolate',
-        icon: 'just-chocolate',
-        price: 119,
-        description: 'For chocolate lovers',
-        promo: true
-      },
-      {
-        id: 'adsd234',
-        name: 'Glazed Fudge',
-        icon: 'glazed-fudge',
-        price: 129,
-        description: 'Perfect for all',
-      },
-      {
-        id: 'as1456hgf4',
-        name: 'Caramel Swirl',
-        icon: 'caramel-swirl',
-        price: 129,
-        description: 'Chocolate with caramel swirl'
-      }
+      // {
+      //   id: 'as1234',
+      //   name: 'Chocolate',
+      //   icon: 'just-chocolate',
+      //   price: 119,
+      //   description: 'For chocolate lovers',
+      //   promo: true
+      // },
+      // {
+      //   id: 'adsd234',
+      //   name: 'Glazed Fudge',
+      //   icon: 'glazed-fudge',
+      //   price: 129,
+      //   description: 'Perfect for all',
+      // },
+      // {
+      //   id: 'as1456hgf4',
+      //   name: 'Caramel Swirl',
+      //   icon: 'caramel-swirl',
+      //   price: 129,
+      //   description: 'Chocolate with caramel swirl'
+      // }
     ]
   }
 }
