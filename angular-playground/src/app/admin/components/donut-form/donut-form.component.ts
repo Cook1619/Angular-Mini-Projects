d022835ae15d796c4028fdfec499e6dbd9ae7d16
Cook1619 <cook6348@gmail.com>
2023-04-04 08:39:29 -0500

    Setting up fields with ngModel to help us keep track of state in a form

--- angular-playground/src/app/admin/components/donut-form/donut-form.component.ts
+++ angular-playground/src/app/admin/components/donut-form/donut-form.component.ts
@@ -3,8 +3,17 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'donut-form',
   template: `
-    // registers our form as a angular form
+<!-- Registers our form as a angular form-->
+<!-- Creating an #form=ngModel helps keep track of whatever you add the ngModel to a creates an object for us-->
     <form class="donut-form" #form="ngForm">
+      <label>
+        <span>Name</span>
+        <input type="text" name="name" class="input" ngModel>
+      </label>
+      <label>
+        <span>Price</span>
+        <input type="number" name="price" class="input" ngModel>
+      </label>
       <pre>{{ form.value | json }}</pre>
     </form>
   `,
