467a2eead17cf57673a84712c55904873f9c894c
Cook1619 <cook6348@gmail.com>
2023-04-05 07:48:43 -0500

    using textarea with ngModel

--- angular-playground/src/app/admin/components/donut-form/donut-form.component.ts
+++ angular-playground/src/app/admin/components/donut-form/donut-form.component.ts
@@ -42,6 +42,15 @@ import {Component} from '@angular/core';
           <span>Limited</span>
         </label>
       </div>
+
+      <label>
+        <span>Description</span>
+        <textarea
+          name="description"
+          class="input input--textarea"
+          ngModel>
+        </textarea>
+      </label>
       <pre>{{ form.value | json }}</pre>
     </form>
   `,
